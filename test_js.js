7d26a20e0d6b6dd31b85710c5991f1b635b88163
TwinHter <nhdnguyen05@gmail.com>
2024-03-09 00:54:43 +0700

    .

--- test_js.js
+++ test_js.js
@@ -1,3 +1,31 @@
-<div>
-  
-</div>
+//Styling element
+// const title = document.querySelector('header');
+// title.style.color = 'red';
+// const listItems = document.querySelectorAll('.item');
+// listItems.forEach(item => {
+//   item.style.fontSize = '2rem';
+// });
+//Create Element
+const ul = document.querySelector('.items');
+const li = document.createElement('li');
+//Adding Elemet
+ul.append(li);
+//Modifying context
+
+// const firstListItem = document.querySelector('.item');
+// console.log(firstListItem.innerText);
+// console.log(firstListItem.innerHTML);
+// console.log(firstListItem.textContent);
+li.innerText = 'abc';
+
+//Modifying Attributes and Classes
+li.setAttribute('class', 'item');
+li.removeAttribute('class');
+
+// const title = document.querySelector('#main-heading');
+// console.log(title.getAttribute('id'));
+li.classList.add('item');
+console.log(li.classList.contains('item'));
+
+//Remove Element
+li.remove();
---
+++ test_js_dom.js
@@ -0,0 +1,32 @@
+// DOM manipulation
+
+// Traverse the DOM
+
+// Parent Node Traverse
+let ul = document.querySelector('.items');
+ul.style.fontSize = '2rem';
+// console.log(ul);
+// console.log(ul.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement);
+
+// const html = document.documentElement;
+// console.log(html);
+// console.log(html.parentElement); // tra ve null vi document khong phai la 1 element (not <></>)
+// console.log(html.parentNode); // tra ve document vi theo so do cua cay DOM => nen dung
+
+// Child Node Traverse
+
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+// ul.childNodes[1].style.backgroundColor = 'yellow';
+// console.log(ul.children);
+// console.log(ul.firstElementChild);
+// console.log(ul.lastElementChild);
+
+// Sibling Node Traverse
+const div = document.querySelector('.container');
+console.log(div.childNodes);
+console.log(ul.previousSibling);
+console.log(ul.previousElementSibling)
+console.log(ul.nextSibling);
